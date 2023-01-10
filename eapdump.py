f1e2080ab86fb5d092014dca830d6635257b4398
tanupoo <github-shoichi@tanu.org>
2023-01-11 01:21:09 +0900

    changed to use pydantic.

--- eapdump.py
+++ eapdump.py
@@ -2,114 +2,128 @@
 1673167573.015110: RX EAPOL - hexdump(len=9): 02 00 00 05 01 2b 00 05 01
 """
 
+from pydantic import BaseModel
+from typing import List, Optional
 import re
 import argparse
+
 re_eap_hexdump = re.compile("(.*)(RX|TX) EAPOL - hexdump\(len=(\d+)\): ([a-f\d\s]+)")
+
+class ValText(BaseModel):
+    Value: int
+    Text: str
+
+    def __str__(self):
+        return f"{self.Value} ({self.Text})"
+
+class EAPOL(BaseModel):
+    Version: int
+    Type: ValText
+    Length: int
+    Body: List[str]
+
+class EAP(BaseModel):
+    Code: ValText
+    Identifier: int
+    Length: int
+    Type: Optional[ValText]
+    Data: Optional[List[str]]
 
 def com_int(vs):
     if isinstance(vs, list):
         return int("".join(vs), 16)
     return int(vs, 16)
 
-class tv():
-    v = None
-    t = ""
-    def __init__(self, v, t=""):
-        self.v = v
-        self.t = "UNKNOWN" if t is None else t
-    def __str__(self):
-        return f"{self.t} ({self.v})"
+def com_vt(vs: str, vtmap: dict):
+    v = com_int(vs)
+    t = vtmap.get(v)
+    return ValText(
+            Value=v,
+            Text="UNKNOWN" if t is None else t
+            )
 
-def parse_eapol(data, verbose=False):
-    """
-    EAPOL parser
-    """
-    def eapol_type(vs):
-        v2t_map = {
-                0: "EAP Packet",
-                1: "Start",
-                2: "Logoff",
-                3: "Key",
-                4: "ASF Alert",
-                }
-        v = com_int(vs)
-        t = v2t_map.get(v)
-        return tv(v, t)
-    p = type("EAPOL", (), {
-        "ver": com_int(data[0]),
-        "type": eapol_type(data[1]),
-        "len": com_int(data[2:4]),
-        "body": data[4:], # str
-        })
-    if verbose:
+def parse_eapol(data, verbosity=0):
+    # EAPOL parser
+    type_map = {
+            0: "EAP Packet",
+            1: "Start",
+            2: "Logoff",
+            3: "Key",
+            4: "ASF Alert",
+            }
+    p = EAPOL(
+            Version=com_int(data[0]),
+            Type=com_vt(data[1], type_map),
+            Length=com_int(data[2:4]),
+            Body=data[4:]
+        )
+    if verbosity:
         print(f"## EAPOL")
-        print(f"- Version: {p.ver}")
-        print(f"- Type: {p.type}")
-        print(f"- Length: {p.len}")
-        print(f"- Body: {p.body}")
+        print(f"- Version: {p.Version}")
+        print(f"- Type: {p.Type}")
+        print(f"- Length: {p.Length}")
+        if verbosity > 1:
+            print(f"- Body: {p.Body}")
     return p
 
-def parse_eap(data, verbose=False):
-    """
-    EAP parser
-    """
-    def eap_type(vs):
-        v2t_map = {
-                1: "Identity",
-                2: "Notification",
-                3: "NAK",
-                4: "MD5-Challenge",
-                5: "OTP",
-                6: "GTC",
-                13: "EAP-TLS",
-                21: "EAP-TTLS",
-                25: "EAP-PEAP",
-                }
-        v = com_int(vs)
-        t = v2t_map.get(v)
-        return tv(v, t)
-    def eap_code(vs):
-        v2t_map = {
-                1: "Request",
-                2: "Response",
-                3: "Success",
-                4: "Failure",
-                }
-        v = com_int(vs)
-        t = v2t_map.get(v)
-        return tv(v, t)
+def parse_eap(data, verbosity=0):
+    # EAP parser
+    code_map = {
+            1: "Request",
+            2: "Response",
+            3: "Success",
+            4: "Failure",
+            }
+    type_map = {
+            1: "Identity",
+            2: "Notification",
+            3: "NAK",
+            4: "MD5-Challenge",
+            5: "OTP",
+            6: "GTC",
+            13: "EAP-TLS",
+            21: "EAP-TTLS",
+            25: "EAP-PEAP",
+            }
     # EAP Header
-    p = type("EAP", (), {
-        "code": eap_code(data[0]),
-        "id": data[1],
-        "len": com_int(data[2:4]),
-    })
-    if p.len > 4:
-        p.type = eap_type(data[4])
-        p.data = data[5:]
-    if verbose:
+    p = EAP(
+            Code=com_vt(data[0], code_map),
+            Identifier=com_int(data[1]),
+            Length=com_int(data[2:4]),
+            )
+    if p.Length > 4:
+        p.Type = com_vt(data[4], type_map)
+        if p.Type.Text == "Identity":
+            p.Data = "".join([chr(int(i,16)) for i in data[5:]])
+        else:
+            p.Data = data[5:]
+    if verbosity:
         print(f"## EAP")
-        print(f"- Code: {p.code}")
-        print(f"- Identifier: {p.id}")
-        print(f"- Length: {p.len}")
-        if p.len > 4:
-            print(f"- Type: {p.type}")
-            print(f"- Data: {p.data}")
+        print(f"- Code: {p.Code}")
+        print(f"- Identifier: {p.Identifier}")
+        print(f"- Length: {p.Length}")
+        if p.Length > 4:
+            print(f"- Type: {p.Type}")
+            print(f"- Data: {p.Data}")
     return p
 
-def parse_eapol_hexdump(payload):
+def parse_eapol_hexdump(payload, verbosity=0):
     """
         "02 00 ..."
     """
     d = payload.split()
-    eapol = parse_eapol(d, verbose=True)
-    if eapol.type.v != 0:
-        return
-    eap = parse_eap(eapol.body, verbose=True)
+    eapol = parse_eapol(d, verbosity=verbosity)
+    if eapol.Type.Value != 0:
+        return eapol
+    eap = parse_eap(eapol.Body, verbosity=verbosity)
 
 ap = argparse.ArgumentParser()
 ap.add_argument("log_file")
+ap.add_argument("-v", action="append_const", default=[], const=1,
+                dest="_verbosity")
 opt = ap.parse_args()
+
+opt.verbosity = len(opt._verbosity)
 
 if opt.log_file == "-":
     fd = sys.stdin
@@ -124,6 +138,7 @@ for line in fd:
         ts = r.group(1)
         dir = r.group(2)
         len = r.group(3)
-        print("===")
-        print("#", ts, dir, len)
-        parse_eapol_hexdump(r.group(4))
+        if opt.verbosity:
+            print("===")
+            print("#", ts, dir, len)
+        parse_eapol_hexdump(r.group(4), opt.verbosity)
